58c24b06dd74c5b0b8929a83b35a731f69ae4126
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-19 14:15:15 -0500

    refactor: Update server configuration to use config module for port and error handling

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const tweetsRouter = require("./routes/tweetsRouter.js")
+const config = require("./config/index");
+const tweetsRouter = require("./routes/tweetsRouter")
+const { logErrors, wrapErrors, errorHandler } = require('./utils/middlewares/errorMiddlewares');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = config.port;
 
 app.use(express.json());
 app.use("/tweets", tweetsRouter);
+
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`SERVER IS RUN ON PORT http://localhost:${PORT}`))
